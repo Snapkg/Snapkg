8d117839a3166a393c77ac9206bd231afda4f354
elfin8er <elfin8er@gmail.com>
2024-02-03 17:38:52 -0500

    Update new_issue_label_pkg_req_new.js

--- .github/workflows/new_issue_label_pkg_req_new.js
+++ .github/workflows/new_issue_label_pkg_req_new.js
@@ -1,4 +1,18 @@
 // Get Markdown content from command line argument
+const axios = require('axios');
+
 const issue_number = process.argv[2];
 
-console.log(issue_number);
+// Replace 'https://example.com' with the URL of the website you want to fetch
+const url = 'https://api.github.com/repos/Snapkg/snapkg/issues/' + issue_number;
+
+// Make a GET request to the website
+axios.get(url)
+  .then(response => {
+    // Log the HTML content of the website
+    console.log(response.data);
+  })
+  .catch(error => {
+    // Handle errors, if any
+    console.error('Error fetching website:', error.message);
+  });
